5d7efdde27afd6734ac0b00ebc24510041fd02a7
Madeline Morton <madelinemorton614@gmail.com>
2019-05-17 19:21:18 -0500

    buttons up. not working yet

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,28 +1,76 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './feeling.css';
+// import PropTypes from 'prop-types';
+// import { withStyles } from '@material-ui/core/styles';
+// import classnames from 'classnames';
+// import Card from '@material-ui/core/Card';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import CardActions from '@material-ui/core/CardActions';
+// import IconButton from '@material-ui/core/IconButton';
+// import red from '@material-ui/core/colors/red';
+// import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+// import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+// import Input from '@material-ui/core/Input';
+import { Button } from '@material-ui/core';
+
+// const styles = theme => ({
+//     card: {
+//         maxWidth: 600,
+//     },
+//     media: {
+//         height: 0,
+//         paddingTop: '56.25%', // 16:9
+//     },
+//     actions: {
+//         display: 'flex',
+//     }
+// });
+
 
 class Feeling extends Component {
-
-
-
-
-
     render(){
-        return (
-            <div>
-            <h2>How's THIS Make You Feel?!</h2>
-                {this.props.reduxState.images.map(image => {
-                    return (
-                        <img key={image.id} src={image.path} alt={image.title} className="image" />
-                    )
-                })}
-            
-            </div>
-        )
-    }
-}
+      return(
+          <div>
+          <h2>Images Here:</h2>
+          
+              <h2>How's THIS Make You Feel?!</h2>
+              {this.props.reduxState.images.map(image => {
+                  return (
+                      <div className="imageDiv">
+                      <Button variant="contained" color="primary">Backward</Button>
+                      <img key={image.id} src={image.path} alt={image.title} className="feelingImg" />
+                      <Button variant="contained" color="primary">Forward</Button>
+                      </div>
+                      )
+              })}
+          </div>
+      )}
+    }  
+//         {this.props.reduxState.images.map(image => {
+//         return (
+//             <div>
+//             <Card className="image">
 
-
+//                 <CardMedia
+//                     image="Image path"
+//                     title="IMAGE PATH"
+//                 />
+//                 <Input placeholder="Test"></Input>
+//                 <CardActions disableActionSpacing>
+//                     <IconButton aria-label="Arrow Back">
+//                         <ArrowBackIcon />
+//                     </IconButton>
+//                     <IconButton aria-label="Arrow Forward">
+//                         <ArrowForwardIcon />
+//                     </IconButton>
+//                 </CardActions>
+//             </Card>
+//             </div>
+//         ));
+        
+//     })
+//   }}
 
 
 
